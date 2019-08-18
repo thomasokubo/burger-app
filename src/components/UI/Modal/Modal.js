2aac0b060249734b37ff1c4d8e61cfbea091ef6d
Thomas Okubo <tai.okubo93@gmail.com>
2019-08-18 14:20:57 -0300

    Update Modal component
    Update shouldComponentUpdate hook to improve performance

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.module.css'
 import Wrapper from '../../../hoc/wrapper';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => {
-    if(props.visible) {
-        return (
-            <Wrapper>
-                <Backdrop show={props.visible} clicked={props.closeModal}/>
-                <div className={classes.Modal}>
-                    {props.children}
-                </div>
-            </Wrapper>
-        );
+class Modal extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.visible !== this.props.visible;
     }
-    return null;
+
+    render() {  
+        if(this.props.visible) {
+            return (
+                <Wrapper>
+                    <Backdrop show={this.props.visible} clicked={this.props.closeModal}/>
+                    <div className={classes.Modal}>
+                        {this.props.children}
+                    </div>
+                </Wrapper>
+            );
+        }
+        return null;
+    }
 }
 
-
-export default modal;
+export default Modal;
