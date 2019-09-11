7722302d793b3a13ca34b94e512e1233b8babe7e
Thomas Okubo <tai.okubo93@gmail.com>
2019-09-11 00:58:19 -0300

    Add rect-router-dom
    This library has the following components: BrowserRouter, Route, Switch

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout';
 import Layout from './components/Layout/Layout'
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Layout>
-        <BurgerBuilder></BurgerBuilder>
-        <Checkout />
+          <Switch>
+            <Route path="/" exact component={BurgerBuilder} />
+            <Route path="/checkout" component={Checkout} />
+          </Switch>
       </Layout>
     </div>
   );
